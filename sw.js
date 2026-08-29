13ae05b685d309b903d2a4532c1001da8f718074
mz-dev-cloud <pastfriworld@gmail.com>
2026-08-29 19:35:19 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'Notificação', {
+      body: data.body || '',
+      icon: '/favicon.ico',
+      data: { url: data.url || '/' }
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const url = event.notification.data?.url || '/';
+  event.waitUntil(clients.openWindow(url));
+});
